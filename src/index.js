ada8391841a58e9fa3dcc7f83ba8d9371b296386
Bestie-hagar <hagarshmaia@gmail.com>
2024-12-25 16:21:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-// יצירת root בגרסת React 18
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
